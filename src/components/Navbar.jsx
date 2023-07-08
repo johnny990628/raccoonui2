e8b8a77a10966f1fca150c2dceff2cd0663425d7
李柏勳 <80705123+johnny990628@users.noreply.github.com>
2023-07-09 00:43:46 +0800

    Init setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { PageProps } from './../../.next/types/app/layout'
+import Image from 'next/image'
+import { buttonVariants } from './ui/button'
+
+const Navbar = async () => {
+    return (
+        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-200 border-b border-zinc-200 z-[10] py-2">
+            <div className="container max-w-7xl h-full mx-auto flex item-center justify-between gap-2">
+                <Link href="/" className="flex gap-2 items-center">
+                    <Image src="/logo.svg" alt="logo" width={20} height={20} className="h-8 w-8 sm:h-6 sm:w-6" />
+                    <p className="hidden text-zinc-700 text-sm font-medium md:block">RACCOON</p>
+                </Link>
+
+                {/* search bar */}
+
+                <Link href="/sign-in" className={buttonVariants()}>
+                    Sign In
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
