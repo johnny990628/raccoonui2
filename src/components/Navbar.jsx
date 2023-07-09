976dc81028a94bc28a7767e3b8d399838c04915b
李柏勳 <80705123+johnny990628@users.noreply.github.com>
2023-07-09 21:17:21 +0800

    Style : navbar py2 to 4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import { buttonVariants } from './ui/button'
 
 const Navbar = async () => {
     return (
-        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-200 border-b border-zinc-200 z-[10] py-2">
+        <div className="fixed top-0 inset-x-0 h-fit z-[10] py-4">
             <div className="container max-w-7xl h-full mx-auto flex item-center justify-between gap-2">
                 <Link href="/" className="flex gap-2 items-center">
                     <Image src="/logo.svg" alt="logo" width={20} height={20} className="h-8 w-8 sm:h-6 sm:w-6" />
-                    <p className="hidden text-zinc-700 text-sm font-medium md:block">RACCOON</p>
+                    <p className="hidden  text-sm font-medium md:block">RACCOON</p>
                 </Link>
 
                 {/* search bar */}
